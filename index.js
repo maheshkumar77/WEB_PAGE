546cf8cd2983a7294635b9b9a7bfe0bda5aa9b00
maheshkumar77 <149643073+maheshkumar77@users.noreply.github.com>
2025-03-12 23:43:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const {Client}=require ('pg');
+const express =require ('express');
+const app=express();
+app.use(express.json());
+
+const con=new Client({
+    host:'localhost',
+    user:'postgres',
+    port:'5432',
+    password:'root123',
+    database:'mydata'
+})
